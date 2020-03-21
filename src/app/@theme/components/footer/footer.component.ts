c68f7084e2ccf045e629cf4587f22c7417caf021
rahulnm <rahulnm@gmail.com>
2020-03-21 08:59:34 +0100

    Footer tag changes

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Copyright © <b><a href="#" target="_blank">Energy Potential Tech Solutions</a></b> 2019-20</span>
+    <span class="created-by">Copyright © <b><a href="#" target="_blank">EP Tech Solutions (UK)</a></b> 2019-20</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
