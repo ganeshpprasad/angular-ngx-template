bf240ecd6804212b568dbca26738166b90daed18
Rahul Mudliar <rahul@ep-techsolutions.com>
2020-08-12 19:26:36 +0200

    Fix customer search routing

--- src/app/pages/customer/customer-routing.module.ts
+++ src/app/pages/customer/customer-routing.module.ts
@@ -1,34 +1,30 @@
-import { CustomerSearchComponent } from './customer-search/customer-search.component';
-import { CustomerDetailsComponent } from './customer-details/customer-details.component';
-import { CustomerComponent } from './customer.component';
+import {CustomerSearchComponent} from './customer-search/customer-search.component';
+import {CustomerDetailsComponent} from './customer-details/customer-details.component';
+import {CustomerComponent} from './customer.component';
 
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: '',
-		component: CustomerComponent,
-		children: [
-			{
-				path: 'id',
-				component: CustomerDetailsComponent,
-			},
-			{
-				path: 'search',
-				component: CustomerSearchComponent,
-			},
-			{
-				path: '**',
-				redirectTo: 'search',
-				pathMatch: 'full',
-			},
-		],
-	},
+    {
+        path: '',
+        component: CustomerComponent,
+        children: [
+            {
+                path: ':id',
+                component: CustomerDetailsComponent,
+            },
+            {
+                path: '',
+                component: CustomerSearchComponent,
+            },
+        ],
+    },
 ];
 
 @NgModule({
-	imports: [RouterModule.forChild(routes)],
-	exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class CustomerRoutingModule {}
+export class CustomerRoutingModule {
+}
