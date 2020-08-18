42c870353b7e2ea91a338618fafd9f932f6f9225
Subramanya <subramanyas615@gmail.com>
2020-08-19 00:32:30 +0530

    service bill invoice smart table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,52 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 // import nebular theme modules
 import {
-    NbDatepickerModule,
-    NbDialogModule,
-    NbMenuModule,
-    NbSidebarModule,
-    NbToastrModule,
-    NbWindowModule,
-} from '@nebular/theme';
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbToastrModule,
+  NbWindowModule,
+} from "@nebular/theme";
 
-import {CoreModule} from './@core/core.module';
+import { CoreModule } from "./@core/core.module";
 // tslint:disable-next-line:comment-format
-import {ProvidersModule} from './@providers/providers.module'; //APIs
-import {ThemeModule} from './@theme/theme.module'; // Theme config
-import {AppRoutingModule} from './app-routing.module'; // Routing
-import {AppComponent} from './app.component';
-import {NbEvaIconsModule} from '@nebular/eva-icons';
+import { ProvidersModule } from "./@providers/providers.module"; //APIs
+import { ThemeModule } from "./@theme/theme.module"; // Theme config
+import { AppRoutingModule } from "./app-routing.module"; // Routing
+import { AppComponent } from "./app.component";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
 
 // import { CustomerComponent } from './customer/customer.component';
 // import { TariffComponent } from './tariff/tariff.component';
 // import { AssetDetailsComponent } from './asset-details/asset-details.component'; // Component
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        FormsModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        ThemeModule.forRoot(),
-        NbSidebarModule.forRoot(),
-        NbMenuModule.forRoot(),
-        NbDatepickerModule.forRoot(),
-        NbDialogModule.forRoot(),
-        NbWindowModule.forRoot(),
-        NbToastrModule.forRoot(),
-        NbDialogModule.forRoot(),
-        NbDatepickerModule.forRoot(),
-        CoreModule.forRoot(),
-        ProvidersModule.forRoot(),
-        NbEvaIconsModule,
-    ],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    ThemeModule.forRoot(),
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbToastrModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    CoreModule.forRoot(),
+    ProvidersModule.forRoot(),
+    NbEvaIconsModule,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/pages/srv-account/srv-account-details/srv-account-details.component.ts
+++ src/app/pages/srv-account/srv-account-details/srv-account-details.component.ts
@@ -1,56 +1,136 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import {flatMap, map} from 'rxjs/operators';
-import {IServiceAccountDetails, IServiceAccountDetailsAPIService} from '../../../@providers/data/service-account';
-import {IBillInvoiceDetails, IBillInvoiceDetailsAPIService} from '../../../@providers/data/bill-invoice';
+import { LocalDataSource } from "ng2-smart-table";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { Observable } from "rxjs";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { flatMap, map } from "rxjs/operators";
+import {
+  IServiceAccountDetails,
+  IServiceAccountDetailsAPIService,
+} from "../../../@providers/data/service-account";
+import {
+  IBillInvoiceDetails,
+  IBillInvoiceDetailsAPIService,
+} from "../../../@providers/data/bill-invoice";
 
 @Component({
-    selector: 'ngx-srv-account',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    templateUrl: './srv-account-details.component.html',
-    styleUrls: ['./srv-account-details.component.scss'],
+  selector: "ngx-srv-account",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./srv-account-details.component.html",
+  styleUrls: ["./srv-account-details.component.scss"],
 })
 export class SrvAccountDetailsComponent implements OnInit {
-    @Input() service_id: string;
+  @Input() service_id: string;
 
-    routed_id$: Observable<string>;
-    serviceAccount: Observable<IServiceAccountDetails>;
-    billInvoices: Observable<IBillInvoiceDetails[]>;
+  routed_id$: Observable<string>;
+  serviceAccount: Observable<IServiceAccountDetails>;
+  billInvoices: Observable<IBillInvoiceDetails[]>;
+  localDataSource: LocalDataSource = new LocalDataSource();
+  tableSettings = {
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    columns: {
+      invoice_id: {
+        title: "Invoice ID",
+        type: "string",
+        editable: false,
+        // renderComponent: CustomerDetailsTableRouteComponent,
+      },
+      date_issued: {
+        title: "Date Issued",
+        type: "string",
+        editable: false,
+      },
+      total_due_id: {
+        title: "Total Due ID",
+        type: "string",
+        editable: false,
+      },
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private billInvoiceDetailsAPIService: IBillInvoiceDetailsAPIService,
-        private serviceDetailsAPIService: IServiceAccountDetailsAPIService) {
-    }
+      open_bal: {
+        title: "Open Balance",
+        type: "string",
+        editable: false,
+      },
 
-    ngOnInit() {
-        this.routed_id$ = this.route.paramMap.pipe(
-            map((params: ParamMap) => params.get('id')),
-        );
-        this.serviceAccount = this.routed_id$
-            .pipe(
-                flatMap(id => this.serviceDetailsAPIService.getServiceAccountDetailsByID(id)),
-            );
-        this.billInvoices = this.serviceAccount
-            .pipe(
-                flatMap(service_ac =>
-                    this.billInvoiceDetailsAPIService.getBillInvoiceDetailsByServiceID(service_ac.service_id)),
-            );
-    }
+      gross: {
+        title: "Gross",
+        type: "string",
+        editable: false,
+      },
+      net: {
+        title: "Net",
+        type: "string",
+        editable: false,
+      },
+      vat: {
+        title: "Vat",
+        type: "string",
+        editable: false,
+      },
+    },
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private billInvoiceDetailsAPIService: IBillInvoiceDetailsAPIService,
+    private serviceDetailsAPIService: IServiceAccountDetailsAPIService
+  ) {}
 
-    onEventClick() {
-        return;
-    }
+  ngOnInit() {
+    this.routed_id$ = this.route.paramMap.pipe(
+      map((params: ParamMap) => params.get("id"))
+    );
+    this.serviceAccount = this.routed_id$.pipe(
+      flatMap((id) =>
+        this.serviceDetailsAPIService.getServiceAccountDetailsByID(id)
+      )
+    );
+    this.billInvoices = this.serviceAccount.pipe(
+      flatMap((service_ac) =>
+        this.billInvoiceDetailsAPIService.getBillInvoiceDetailsByServiceID(
+          service_ac.service_id
+        )
+      )
+    );
 
-    onClickBack() {
-        this.router.navigate(['../', {}], {relativeTo: this.route});
-    }
+    this.billInvoices.subscribe((data) => {
+      this.loadSearchResult(data);
+    });
+  }
 
-    onClickHome() {
-        this.router.navigate(['/pages/landing', {}], {relativeTo: this.route});
-    }
+  private loadSearchResult(searchResult: IBillInvoiceDetails[]) {
+    const table_data = searchResult.map((billInvoice) => {
+      return {
+        invoice_id: billInvoice.invoice_id,
+        date_issued: billInvoice.date_issued,
+        total_due_id: billInvoice.total_due,
+        open_balance: billInvoice.open_bal,
+        gross: billInvoice.gross,
+        net: billInvoice.net,
+        vat: billInvoice.vat,
+      };
+    });
+    this.localDataSource.load(table_data);
+  }
 
+  onEventClick() {
+    return;
+  }
+
+  onClickBack() {
+    this.router.navigate(["../", {}], { relativeTo: this.route });
+  }
+
+  onClickHome() {
+    this.router.navigate(["/pages/landing", {}], { relativeTo: this.route });
+  }
 }
